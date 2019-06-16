48297faa929c5cd679a1e9ddc51f902516d59424
SQ8KFH <sq8kfh@gmail.com>
2019-06-16 16:45:12 +0200

    cmake lib fix + add readline

--- h9cli/h9cli.cc
+++ h9cli/h9cli.cc
@@ -8,8 +8,13 @@
 
 #include "config.h"
 #include <cstdlib>
+#include <cstdio>
+
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #include "common/clientctx.h"
+
 
 int main(int argc, char **argv)
 {
@@ -17,6 +22,23 @@ int main(int argc, char **argv)
 
     auto res = ctx.parse_options(argc, argv);
     ctx.load_configuration(res);
+
+    while (1) {
+        // Display prompt and read input
+        char* input = readline("h9> ");
+
+        // Check for EOF.
+        if (!input)
+            break;
+
+        // Add input to readline history.
+        add_history(input);
+
+        // Do stuff...
+
+        // Free buffer that was allocated by readline
+        free(input);
+    }
 
     return EXIT_SUCCESS;
 }
